56d5a54fbd9f0dd771fd59768cb9f02e8eedcbbf
elaineay <elaineauyang@hotmail.com>
2019-03-10 05:24:49 -0700

    lilypad backend logic

---
+++ lilypad/lilypadclass.py
@@ -0,0 +1,7 @@
+class Lilypad:
+    def __init__(self, name, latlon, type, free, comments):
+        self.name = name;
+        self.latlon = latlon;
+        self.type = type;
+        self.free = free;
+        self.comments = comments;
