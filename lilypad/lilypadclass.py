99a7412e6fe9e7c338e40e87de49b6524fe6360a
elaineay <elaineauyang@hotmail.com>
2019-03-10 01:02:33 -0800

    added name column

--- lilypad/lilypadclass.py
+++ lilypad/lilypadclass.py
@@ -1,5 +1,6 @@
 class Lilypad:
-    def __init__(self, latlon, pad, free, comments):
+    def __init__(self, name, latlon, pad, free, comments):
+        self.name = name;
         self.latlon = latlon;
         self.pad = pad;
         self.free = free;
